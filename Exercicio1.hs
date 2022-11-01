a9d2b34578d8fbccbe755b83030ab17cf5811c7e
Pedro Belle <99338512+PBelle451@users.noreply.github.com>
2022-11-01 10:46:27 -0300

    Delete Exercicio1.hs

--- Exercicio1.hs
+++
@@ -1,6 +0,0 @@
-module Exercicio1 where
-    --Exercicio 1:
-    passagem idade valor
-        | idade < 2 = valor * 0.1
-        | idade >= 60 = valor * 0.60
-        | idade > 2 && idade <= 10 = valor * 0.5
